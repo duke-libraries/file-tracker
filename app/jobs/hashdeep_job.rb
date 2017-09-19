6b2a0f9691f89b0739476a44a7306a759c79c19a
David Chandek-Stark <dchandekstark@gmail.com>
2017-09-19 12:30:20 -0400

    Changed params of HashdeepJob.

--- app/jobs/hashdeep_job.rb
+++ app/jobs/hashdeep_job.rb
@@ -2,17 +2,20 @@ class HashdeepJob < ApplicationJob
 
   queue_as :hashdeep
 
-  def perform(work_dir, files)
+  def perform(dir)
     data = []
-    command = [ "hashdeep", "-s", "-c", "md5,sha1" ] + files
-    IO.popen(command, chdir: work_dir) do |io|
+    files = ( Dir.entries(dir) - ['.', '..'] ).select { |f| File.file?(File.absolute_path(f, dir)) }
+    hashdeep = %w( hashdeep -s -c md5,sha1 ) + files
+
+    IO.popen(hashdeep, chdir: dir) do |io|
       io.each do |line|
         next unless line =~ /^\d/ # skip hashdeep header
         size, md5, sha1, path = line.chomp.split(/,/, 4)
         data << { path: path, size: size.to_i, md5: md5, sha1: sha1 }
       end
     end
-    TrackedFile.import(data)
+
+    TrackedFile.import(data) if data.present?
   end
 
 end
--- app/jobs/track_children_job.rb
+++ app/jobs/track_children_job.rb
@@ -4,22 +4,15 @@ class TrackChildrenJob < ApplicationJob
 
   def perform(path)
     children = Dir.entries(path) - ['.', '..']
-    files = []
 
     children.each do |child|
       abspath = File.absolute_path(child, path)
-      if File.file?(abspath)
-        files << abspath
-      elsif File.directory?(abspath)
-        TrackChildrenJob.perform_later(abspath) unless Dir.empty?(abspath)
+      if File.directory?(abspath) && !Dir.empty?(abspath)
+        TrackChildrenJob.perform_later(abspath)
       end
     end
 
-    if files.present?
-      tracked_files = TrackedFile.under(path).pluck(:path)
-      new_files = (files - tracked_files).map { |f| File.basename(f) }
-      HashdeepJob.perform_later(path, new_files)
-    end
+    HashdeepJob.perform_later(path)
   end
 
 end
