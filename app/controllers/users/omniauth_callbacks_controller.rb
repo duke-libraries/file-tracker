b1b43dd5154f702802371748886286909974c79e
David Chandek-Stark <dchandekstark@gmail.com>
2017-10-12 17:24:35 -0400

    Fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def shibboleth
     user = resource_class.from_omniauth(request.env["omniauth.auth"])
+    set_flash_message :notice, :success, kind: "Duke NetID"
     sign_in_and_redirect user
   end
 
