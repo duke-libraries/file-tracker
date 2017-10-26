e024816959e54f96afbae72d32cde2440d408f3d
David Chandek-Stark <dchandekstark@gmail.com>
2017-10-26 13:56:25 -0400

    Clears fixity queues before performing new BatchFixityCheckJob.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,6 +2,10 @@ class ApplicationJob
 
   class_attribute :queue
 
+  def self.dequeue_all
+    Resque::Job.destroy(queue, self)
+  end
+
   # See Resque README re: MySQL and Rails 4.x
   # (Rails 5 not mentioned as of 2017-10-20).
   def self.before_perform_00_clear_active_connections(*args)
--- app/jobs/batch_fixity_check_job.rb
+++ app/jobs/batch_fixity_check_job.rb
@@ -1,5 +1,9 @@
 class BatchFixityCheckJob < BatchJob
 
+  def self.before_perform_dequeue_fixity_jobs
+    CheckFixityJob.dequeue_all
+  end
+
   def self.perform(max = nil)
     BatchFixityCheck.call(max)
   end
