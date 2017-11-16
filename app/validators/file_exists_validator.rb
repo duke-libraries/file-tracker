be942cc9b87d934afedce7f3f8677d1ee6049080
David Chandek-Stark <dchandekstark@gmail.com>
2017-11-16 17:49:37 -0500

    Inventory process ignores empty files.
    Adds validations to file path:
    - File path cannot be a symlink.
    - File cannot be empty.

--- app/validators/file_exists_validator.rb
+++ app/validators/file_exists_validator.rb
@@ -1,8 +1,12 @@
 class FileExistsValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    unless File.file?(value)
-      record.errors[attribute] << (options[:message] || "does not exist or is not a file")
+    if File.file?(value)
+      if File.symlink?(value)
+        record.errors[attribute] << "cannot be a symbolic link"
+      end
+    else
+      record.errors[attribute] << "does not exist or is not a file"
     end
   end
 
