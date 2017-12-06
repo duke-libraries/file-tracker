18332b8632e57787d2aaea1612ecc4cdb2f8a6f8
David Chandek-Stark <dchandekstark@gmail.com>
2017-12-06 18:05:22 -0500

    Fixed upgrade script

--- db/scripts/upgrade_to_v2.0.rb
+++ db/scripts/upgrade_to_v2.0.rb
@@ -6,12 +6,12 @@
 
 puts "Setting tracked_directory_id for each row in tracked_files ..."
 TrackedDirectory.all.each do |dir|
-  files = TrackedFile.where("path LIKE ?", "#{dir.path}/%")
+  files = TrackedFile.where("tracked_directory_id IS NULL and path LIKE ?", "#{dir.path}/%")
   files.update_all(tracked_directory_id: dir.id)
 end
 
 puts "Updating TrackedFile paths to make relative ..."
-TrackedFile.all.each do |file|
+TrackedFile.where("path NOT LIKE '/%'").each do |file|
   file.sanitize_path!
   file.save!
 end
