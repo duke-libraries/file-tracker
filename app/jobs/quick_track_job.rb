5842e2db56aba355666460fd75c090b99c038a19
David Chandek-Stark <david.chandek.stark@duke.edu>
2018-07-17 10:27:59 -0400

    Adds option to QuickTrackJob and related task to limit by directory.

--- app/jobs/quick_track_job.rb
+++ app/jobs/quick_track_job.rb
@@ -1,7 +1,8 @@
 class QuickTrackJob < BatchJob
 
-  def self.perform
-    TrackedFile.find_each do |tf|
+  def self.perform(id = nil)
+    relation = id ? TrackedDirectory.find(id).tracked_files : TrackedFile.all
+    relation.find_each do |tf|
       queue = TrackFileJob.queue_for_tracked_file(tf)
       Resque.enqueue_to(queue, TrackFileJob, tf.path)
     end
