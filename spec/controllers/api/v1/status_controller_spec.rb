97b4b6ac799984c2c7804e3bd57a6fb7fde0024f
David Chandek-Stark <david.chandek.stark@duke.edu>
2018-12-19 13:52:23 -0500

    Version 2.16.1
    Changes to some status field names
    Tests added for StatusController

---
+++ spec/controllers/api/v1/status_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+module Api::V1
+  RSpec.describe StatusController do
+
+    describe "index" do
+      subject { get :index, format: 'json' }
+
+      it { is_expected.to be_successful }
+
+      describe "response content" do
+        before { get :index, format: 'json' }
+        let(:body) { JSON.parse(response.body) }
+
+        describe "meta" do
+          subject { body["meta"] }
+
+          its(["version"]) { is_expected.to eq FileTracker::VERSION }
+        end
+
+        describe "data" do
+          subject { body["data"] }
+
+          its(:keys) { are_expected.to contain_exactly("queues", "directories", "files") }
+        end
+      end
+    end
+
+  end
+end
